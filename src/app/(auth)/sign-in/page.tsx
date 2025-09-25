8b7defc5f472e6829f14dadd2261103c14e893d9
YashShah13 <shahyash161@gmail.com>
2025-09-25 11:53:38 +0530

    05: Authentication Socials

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+import { SignInViews} from "@/modules/auth/ui/views/sign-in-views";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+import { redirect } from "next/navigation";
+
+
+const Page = async () => {
+const session = await auth.api.getSession({
+    headers: await headers(),
+   });
+  
+    if(!!session) {
+      redirect("/");
+    }
+  
+  return<SignInViews />
+  
+}
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import { SignUpViews} from "@/modules/auth/ui/views/sign-up-views";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+const session = await auth.api.getSession({
+    headers: await headers(),
+   });
+  
+    if(!!session) {
+      redirect("/");
+    }
+  
+  return<SignUpViews />
+  
+}
+
+export default Page;
+
