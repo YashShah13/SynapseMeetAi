bb12f171fce5a7d0f253c4125e64964ec834abe4
YashShah13 <shahyash161@gmail.com>
2025-10-12 10:46:26 +0530

    16: meeting setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { pgTable, text, timestamp, boolean} from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, pgEnum} from "drizzle-orm/pg-core";
 import { timeStamp } from "console";
 
 
@@ -61,4 +61,33 @@ export const agents = pgTable("agents",{
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
 
+} )
+
+export const meettingStatus = pgEnum("meeting_status", [
+    "upcoming",
+    "active",
+      "completed",
+      "processing",
+       "canceled"
+]);
+
+export const meetings = pgTable("meetings",{
+    id:text("id")
+      .primaryKey()
+      .$defaultFn(() =>nanoid()),
+    name: text("name").notNull(),
+    userId: text("user_id")
+          .notNull()
+          .references(() =>user.id, {onDelete: "cascade"}),
+     agentId: text("agent_id")
+          .notNull()
+          .references(() =>agents.id, {onDelete: "cascade"}), 
+     status: meettingStatus("status").notNull().default("upcoming"),    
+    startedAt: timestamp("started_at"),  
+    endedAt: timestamp("ended_at"),
+    transcriptUrl: text("transcript_url"),
+    recordingUrl: text("recording_url"),
+    summary: text("summary"),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
 } )
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,11 @@
 import {agentsRouter} from "@/modules/agents/server/procedure";
 import {createTRPCRouter } from '../init';
+import { meetings } from "@/db/schema";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
