b8ac29d65c9c3c9da824f617a85f0edc726f14e9
YashShah13 <shahyash161@gmail.com>
2025-10-19 11:42:08 +0530

    21: meeting variants

---
+++ src/modules/meetings/ui/components/processing-state.tsx
@@ -0,0 +1,13 @@
+import { EmptyState } from "@/components/empty-state";
+
+export const ProcesingState = () => {
+    return (
+        <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
+            <EmptyState
+                image="/Processing-Meeting.png" 
+                title="Meeting Completed"
+                description= "This Meeting was Completed, a summary will appear soon" 
+              />
+        </div>
+    );
+};
